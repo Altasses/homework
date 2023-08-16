0f85748647967f2fbb045c66d26f05da93afeb11
Altasses <kurasov2000@gmail.com>
2023-08-16 19:13:11 +0500

    seminarTaskDemo

---
+++ Seminar2/seminarTaskDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int randomNumber = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число: {randomNumber}");
+
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшее числло: {firstDigit}");
+}
+
+else
+{
+    Console.WriteLine($"Наибольшее числло: {secondDigit}");
+}
+
